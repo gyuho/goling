879c63f92a16660720316ef50bfcfebccbbc8714
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-03 02:49:36 -0800

    Update holing package

---
+++ goling.go
@@ -0,0 +1,4 @@
+// Package goling provides basic features for string manipulation
+// and natural language processing. It also provides spell-check,
+// segmentation, and calculating string similarity algorithms.
+package goling
