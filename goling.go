21f7480e7535e39069e0ba9f0a92da6e9fcb353e
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-06 16:01:54 -0800

    Update trailing comma for godoc

--- goling.go
+++ goling.go
@@ -1,4 +1,4 @@
-// Package goling implements spell-check, segmentation
-// , and string similarity algorithms(Levenshtein Distance
-// , Cosine Similarity).
+// Package goling implements spell-check, segmentation,
+// and string similarity algorithms(Levenshtein Distance,
+// Cosine Similarity).
 package goling
