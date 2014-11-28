e3e2a29552266accb4751cd8dfcdc6b4e5712a5e
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-11-28 05:58:00 -0800

    Add prep

---
+++ prep/prep.go
@@ -0,0 +1,108 @@
+package prep
+
+import (
+	"regexp"
+	"strings"
+)
+
+// CleanUp cleans up unnecessary characters in string.
+// It cleans up the blank characters that carry no meaning in context,
+// converts all white spaces into single whitespace.
+// String is immutable, which means the original string would not change.
+func CleanUp(str string) string {
+	// validID := regexp.MustCompile(`\s{2,}`)
+	// func TrimSpace(s string) string
+	// slicing off all "leading" and
+	// "trailing" white space, as defined by Unicode.
+	str = strings.TrimSpace(str)
+
+	// func Fields(s string) []string
+	// Fields splits the slice s around each instance
+	// of "one or more consecutive white space"
+	slice := strings.Fields(str)
+
+	// now join them with a single white space character
+	return strings.Join(slice, " ")
+}
+
+// Expand expands the apostrophe phrases.
+func Expand(str string) string {
+	// str = strings.Replace(strings.ToLower(str), "'d", " would", -1)
+	str = strings.Replace(str, "'d", " would", -1)
+	str = strings.Replace(str, "isn't", "is not", -1)
+	str = strings.Replace(str, "aren't", "are not", -1)
+	str = strings.Replace(str, "was't", "was not", -1)
+	str = strings.Replace(str, "weren't", "were not", -1)
+
+	str = strings.Replace(str, "'ve", " have", -1)
+	str = strings.Replace(str, "'re", " are", -1)
+	str = strings.Replace(str, "'m", " am", -1)
+	str = strings.Replace(str, "it's", "it is", -1)
+	str = strings.Replace(str, "what's", "what is", -1)
+	str = strings.Replace(str, "'ll", " will", -1)
+
+	str = strings.Replace(str, "won't", "will not", -1)
+	str = strings.Replace(str, "can't", "can not", -1)
+	str = strings.Replace(str, "mustn't", "must not", -1)
+
+	str = strings.Replace(str, "haven't", "have not", -1)
+	str = strings.Replace(str, "hasn't", "has not", -1)
+
+	str = strings.Replace(str, "dn't", "d not", -1)
+	str = strings.Replace(str, "don't", "do not", -1)
+	str = strings.Replace(str, "doesn't", "does not", -1)
+	str = strings.Replace(str, "didn't", "did not", -1)
+
+	return str
+}
+
+// ReplaceNonAlpha replaces all non-alphabetic characters.
+func ReplaceNonAlpha(str, replace string) string {
+	// alphabetic (== [A-Za-z])
+	// \s is a white space character
+	validID := regexp.MustCompile(`[^[:alpha:]\s]`)
+	return validID.ReplaceAllString(str, replace)
+}
+
+// ReplaceNonAlnum replaces all alphanumeric characters.
+func ReplaceNonAlnum(str, replace string) string {
+	// alphanumeric (== [0-9A-Za-z])
+	// \s is a white space character
+	validID := regexp.MustCompile(`[^[:alnum:]\s]`)
+	return validID.ReplaceAllString(str, replace)
+}
+
+// SplitToWords splits string into a slice of words.
+func SplitToWords(str string) []string {
+	str = Expand(str)
+	validID := regexp.MustCompile(`\"`)
+	str = validID.ReplaceAllString(str, "")
+	return strings.Split(CleanUp(str), " ")
+}
+
+// SplitToSentences splits a string into sentences, removing all puctuation.
+// It returns an slice of sentences.
+func SplitToSentences(str string) []string {
+	validID1 := regexp.MustCompile(`\"`)
+	str = validID1.ReplaceAllString(str, "")
+
+	validID2 := regexp.MustCompile(`[.]`)
+	str = validID2.ReplaceAllString(str, ".___")
+
+	validID3 := regexp.MustCompile(`[?]`)
+	str = validID3.ReplaceAllString(str, "?___")
+
+	validID4 := regexp.MustCompile(`[!]`)
+	str = validID4.ReplaceAllString(str, "!___")
+
+	slice := strings.Split(str, "___")
+	// to clean up the empty strings
+	result := []string{}
+	for _, value := range slice {
+		value = CleanUp(value)
+		if value != "" {
+			result = append(result, value)
+		}
+	}
+	return result
+}
---
+++ prep/prep_test.go
@@ -0,0 +1,102 @@
+package prep
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCleanUp(t *testing.T) {
+	str1 := "   Hello,    World! 124  2 This 23is Go project,		It is amazing  . I am excited!    Are you too?    "
+	r1 := CleanUp(str1)
+	if r1 != "Hello, World! 124 2 This 23is Go project, It is amazing . I am excited! Are you too?" {
+		t.Errorf("Expected \"Hello, World! 124 2 This 23is Go project, It is amazing . I am excited! Are you too?\": %s", r1)
+	}
+	str2 := "		Hello World!	This is Go 		"
+	r2 := CleanUp(str2)
+	if r2 != "Hello World! This is Go" {
+		t.Errorf("Expected \"Hello World! This is Go\": %s", r2)
+	}
+	str3 := "	\n\n\n	Hello World! \n\n	This is Go 		"
+	r3 := CleanUp(str3)
+	if r3 != "Hello World! This is Go" {
+		t.Errorf("Expected \"Hello World! This is Go\": %s", r3)
+	}
+}
+
+func TestExpand(t *testing.T) {
+	str := "was't weren't aren't isn't I'd you'd I'll he doesn't Don't I won't I'm You're i've what's up it's I didn't"
+	r := Expand(strings.ToLower(str))
+	rc := "was not were not are not is not i would you would i will he does not do not i will not i am you are i have what is up it is i did not"
+	if r != rc {
+		t.Errorf("Expected\n%v\nbut\n%v", rc, r)
+	}
+}
+
+func TestReplaceNonAlpha(t *testing.T) {
+	str1 := "Hello 1231thi21s123 is 12G33o25!!!!"
+	r1 := ReplaceNonAlpha(strings.ToLower(str1), " ")
+	if r1 != "hello     thi  s    is   g  o      " {
+		t.Errorf("Expected \"hello     thi  s    is   g  o      \": %s", r1)
+	}
+	str2 := "Hello 1231thi21s123 is 12G33o25!!!!"
+	r2 := ReplaceNonAlpha(strings.ToLower(str2), "")
+	if r2 != "hello this is go" {
+		t.Errorf("Expected \"hello this is go\": %s", r2)
+	}
+}
+
+func TestReplaceNonAlnum(t *testing.T) {
+	str1 := "It's me! Hello !@##%W@!#orl!@#!@#!@#d!"
+	r1 := ReplaceNonAlnum(Expand(strings.ToLower(str1)), " ")
+	if r1 != "it is me  hello      w   orl         d " {
+		t.Errorf("Expected \"it is me  hello      w   orl         d \": %s", r1)
+	}
+	str2 := "Hello !@##%W@!#orl!@#!@#!@#d!"
+	r2 := ReplaceNonAlnum(Expand(strings.ToLower(str2)), "")
+	if r2 != "hello world" {
+		t.Errorf("Expected \"hello world\": %s", r2)
+	}
+}
+
+func TestSplitToWords(t *testing.T) {
+	slice1 := SplitToWords("sadf \nsdafsdf a  s  \nsadfsdf")
+	if len(slice1) != 5 {
+		t.Errorf("Expected 5 but %#v", slice1)
+	}
+	str2 := "Hello World!  It is Good to See You, Them, 10. This is Go. How are you? He said \"I am x1000 good.\""
+	slice2 := SplitToWords(strings.ToLower(str2))
+	cslice2 := []string{"hello", "world!", "it", "is", "good", "to", "see", "you,", "them,", "10.", "this", "is", "go.", "how", "are", "you?", "he", "said", "i", "am", "x1000", "good."}
+	if len(slice2) != len(cslice2) {
+		t.Errorf("%#v\n%#v", slice2, cslice2)
+	}
+	for k, v := range slice2 {
+		if cslice2[k] != v {
+			t.Errorf("%#v != %#v", cslice2[k], v)
+		}
+	}
+}
+
+func TestSplitToSentences(t *testing.T) {
+	str1 := "Hello World!  It is Good to See You, Them, 10. This is Go. How are you? He said \"I am x1000 good.\""
+	slice1 := SplitToSentences(str1)
+	cslice1 := []string{"Hello World!", "It is Good to See You, Them, 10.", "This is Go.", "How are you?", "He said I am x1000 good."}
+	if len(slice1) != len(cslice1) {
+		t.Errorf("%#v\n%%#v", slice1, cslice1)
+	}
+	for k, v := range slice1 {
+		if cslice1[k] != v {
+			t.Errorf("%#v != %#v", cslice1[k], v)
+		}
+	}
+	str2 := "Hello World!  It is Good to See You, Them, 10. This is Go. How are you? He said \"I am x1000 good.\""
+	slice2 := SplitToSentences(str2)
+	cslice2 := []string{"Hello World!", "It is Good to See You, Them, 10.", "This is Go.", "How are you?", "He said I am x1000 good."}
+	if len(slice2) != len(cslice2) {
+		t.Errorf("%#v\n%%#v", slice2, cslice2)
+	}
+	for k, v := range slice2 {
+		if cslice2[k] != v {
+			t.Errorf("%#v != %#v", cslice2[k], v)
+		}
+	}
+}
