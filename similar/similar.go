a6e8fcc7afa5d58b12f48d8dab46939b125088a4
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-11-29 01:17:26 -0800

    Add `similar`

--- similar/similar.go
+++ similar/similar.go
@@ -91,3 +91,91 @@ func Cosine(str1, str2 string) float64 {
 	}
 	return float64(numerator) / denominator
 }
+
+// Levenshtein is a string metric for measuring the difference between two sequences.
+// Levenshtein edit distance between two words is the minimum
+// number of single-character edits (insertion, deletion, substitution)
+// required to change one word into the other.
+// The bigger the return value is, the less similar the two texts are
+// because different texts take more edits than similar texts.
+// Insertion, deletion, and substitution each costs 1 edit.
+func Levenshtein(str1, str2 string) float64 {
+
+	// to clear out the unnecessary characters
+	str1 = utils.ReplaceNonAlnum(str1, " ")
+	str2 = utils.ReplaceNonAlnum(str2, " ")
+
+	distance := make2Dslice(len(str1)+1, len(str2)+1)
+
+	// initialize the distance array, with position
+	for y := 0; y <= len(str1); y++ {
+		distance[y][0] = y
+	}
+
+	// initialize the distance array, with position
+	for x := 0; x <= len(str2); x++ {
+		distance[0][x] = x
+	}
+
+	for i := 0; i < len(str1); i++ {
+
+		for j := 0; j < len(str2); j++ {
+
+			// edit cost
+			edit := 1
+			if str1[i] == str2[j] {
+				edit = 0
+			}
+
+			// distance[i][j+1] + 1 : delete/insert from str1
+			// distance[i+1][j] + 1 : delete/insert from str2
+			// distance[i][j] + edit : delete/insert from both
+			distance[i+1][j+1] = getMin(
+				distance[i][j+1]+1,
+				distance[i+1][j]+1,
+				distance[i][j]+edit,
+			)
+		}
+	}
+
+	// equal texts need 0 edit distance
+	if distance[len(str1)][len(str2)] == 0 {
+		// smoothing
+		return 0.0001
+	}
+	return float64(distance[len(str1)][len(str2)])
+}
+
+func getMin(more ...int) int {
+	min := more[0]
+	for _, elem := range more {
+		if min > elem {
+			min = elem
+		}
+	}
+	return min
+}
+
+func make2Dslice(row, column int) [][]int {
+	mat := make([][]int, row)
+	for i := range mat {
+		mat[i] = make([]int, column)
+	}
+	return mat
+}
+
+// Get returns the similarity of two string using Cosing similarity and Levenshtein distance.
+// Higher number means higher similarity. It is up to users to preprossess input texts.
+func Get(str1, str2 string) float64 {
+	// Higher means higher similarity
+	sim1 := Cosine(str1, str2)
+
+	// Lower means higher similarity
+	siml2 := Levenshtein(str1, str2)
+	sim2 := (1.0 / float64(siml2)) * 1000
+
+	siml3 := Levenshtein(utils.DeleteSpace(str1), utils.DeleteSpace(str2))
+	sim3 := (1.0 / float64(siml3)) * 1000
+
+	return math.Sqrt(sim1*sim2*sim3) * 100
+}
